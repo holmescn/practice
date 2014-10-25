1071bfdbd29cec402f20348ea7ef993258a3ff88
Holmes Conan <holmesconan@gmail.com>
2014-10-25 11:15:54 +0800

    Configurable property of C++

---
+++ cpp/property/property.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <iostream>
+
+enum class PropertyType { ReadOnly, WriteOnly, ReadWrite };
+
+template<typename T, typename Friend, PropertyType Type = PropertyType::ReadWrite>
+class Property {
+    T m_val;
+    void Set(const T &val) { m_val = val; }
+    T Get() const { return m_val; }
+public:
+    Property() : m_val( T() ) {}
+
+    // Setter
+    void operator=(const T &val) {
+        static_assert(Type != PropertyType::ReadOnly, "This property is ReadOnly");
+        Set(val);
+    }
+    // Getter
+    operator T() {
+        static_assert(Type != PropertyType::WriteOnly, "This property is WriteOnly");
+        return Get();
+    }
+
+    typedef Friend friend_class;
+    friend friend_class;
+};
+
+class C {
+public:
+    Property<int, C> property1;
+    Property<float, C, PropertyType::ReadOnly> property2;
+    Property<std::string, C, PropertyType::WriteOnly> property3;
+
+    void TestSet() {
+        property2.Set(10.1);
+    }
+
+    void TestGet() {
+        property3.Get();
+    }
+};
+
+int main(int argc, char** argv) {
+    using namespace std;
+
+    C c;
+    c.property1 = 2;
+    int i = c.property1;
+    cout << i << endl;
+
+    // This will emit the static assert
+    // c.property2 = 0.0f;
+    c.TestSet();
+    float f = c.property2;
+    cout << f << endl;
+
+    c.property3 = "AX";
+    // This will emit the static assert
+    //string s = c.property3;
+    //cout << s << endl;
+    c.TestGet();
+}
+
